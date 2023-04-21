2fd75d146b8e4d16d827c7b1983f8d9798f06b1a
bakpark <bakparkbj@gmail.com>
2023-04-21 22:10:08 +0900

    feat: init springboot application

---
+++ src/test/java/bob/ApplicationTests.java
@@ -0,0 +1,12 @@
+package bob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+}
---
+++ src/test/java/com/example/perfserverspring/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perfserverspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
